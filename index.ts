2f48d88154638278d4fa9c26b4de898f91aaf10e
Savera <saveraansari0@gmail.com>
2023-02-03 12:27:51 +0500

    Functionality done and pushed commit

---
+++ index.ts
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+  let convert ={
+
+    "pakrupee":{
+    "usdollar":265.82,
+    "saudiriyal":70.82,
+    "pakrupee":1,
+
+    },
+"usdollar":{
+    "saudiriyal":3.75,
+    "pakrupee":265.80,
+    "usdollar":1,
+},
+    
+
+"saudiriyal":{
+    "pakrupee":70.82,
+    "usdollar":0.27,
+    "saudiriyal":1,
+},
+}
+
+
+
+let answers :{
+
+    from:"pakrupee"|"usdollar"| "saudiriyal"
+
+    to:"pakrupee"|"usdollar"| "saudiriyal"
+    "amount" :number
+}=  await inquirer.prompt([{
+    
+    type : "list",
+    name : "from",
+    choices : [ "pakrupee","usdollar" ,"saudiriyal"],
+    message : "you want to convert your currency from: "},
+{
+    type : "list",
+    name : "to",
+    choices : [ "pakrupee","usdollar" ,"saudiriyal"],
+    message : "you want to convert your currency to: "},
+    {
+    type : "number",
+    name : "amount",
+    
+    message : "plz enter your amount: "}
+
+
+])
+const {from,to,amount}=answers
+ let answer= answers.amount =Math.floor(2)
+// let result= answers.from, answers.to,answers
+ if (answers.from && answers.to && answers.amount) {
+    let convertedAmount= convert
+    [from][to]*amount;
+   
+    console.log(`your's converted amount is ${from} to ${to} is ${convertedAmount}`)
+    
+} else{
+    "insufficent amount"
+}
+
